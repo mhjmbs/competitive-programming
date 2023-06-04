ec5ac854584466f71e8ecd60164beb8178d306f2
mhjmbs <mhjmbs@proton.me>
2023-06-04 18:38:53 -0300

    Add Grid 1 solution

---
+++ solutions/atcoder/educational_dp_contest/grid1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int h, w;
+    cin >> h >> w;
+
+    vector<string> grid(h);
+
+    for(int i = 0; i < h; i++) {
+        cin >> grid[i];
+    }
+
+    vector<vector<int>> dp(h+1, vector<int>(w+1, 0));
+    dp[1][1] = 1;
+
+    for(int i = 1; i <= h; i++) {
+        for(int j = 1; j <= w; j++) {
+            if(grid[i-1][j-1] == '#') continue;
+            if(i != 1 || j != 1) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % int(1e9+7);
+            }
+        }
+    }
+
+    cout << dp[h][w] << '\n';
+}
