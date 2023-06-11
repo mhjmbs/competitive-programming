7aa88c99420ec0a9afab12ff0e6c760d76d88496
mhjmbs <mhjmbs@proton.me>
2023-06-11 20:59:09 -0300

    Add ABCDEFG solution

---
+++ solutions/atcoder/atcoder_beginner_contests/atcoder_beginner_contest_305/abcdefg.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    map<char, int> points= {{'A',0}, {'B',3}, {'C',4}, {'D',8}, {'E',9}, {'F',14}, {'G',23}};
+    
+    char p, q;
+    cin >> p >> q;
+
+    cout << abs(points[p] - points[q]) << '\n';
+}
