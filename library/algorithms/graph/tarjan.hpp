12aacf17d8bcd48a4ee73dc2c82ab852a92a30e7
mhjmbs <mhjmbs@proton.me>
2023-06-29 17:23:56 -0300

    Fix Tarjan's Algorithm lowlink update

--- library/algorithms/graph/tarjan.hpp
+++ library/algorithms/graph/tarjan.hpp
@@ -5,22 +5,21 @@
 
 using namespace std;
 
-void dfs(int node, int &curr_time, vector<vector<int>> &adjacencies, vector<int> &time, vector<int> &lowlink, stack<int> &s, vector<bool> &in_s, vector<vector<int>> &sccs) {
-    lowlink[node] = time[node] = curr_time;
+void dfs(int node, int &curr_time, vector<vector<int>> &adjacencies, vector<int> &tin, vector<int> &lowlink, stack<int> &s, vector<bool> &in_s, vector<vector<int>> &sccs) {
+    lowlink[node] = tin[node] = curr_time;
     s.push(node);
     in_s[node] = true;
 
     for(int adj : adjacencies[node]) {
-        if(time[adj] == -1) {
-            dfs(adj, ++curr_time, adjacencies, time, lowlink, s, in_s, sccs);
-        }
-
-        if(in_s[adj]) {
+        if(tin[adj] == -1) {
+            dfs(adj, ++curr_time, adjacencies, tin, lowlink, s, in_s, sccs);
             lowlink[node] = min(lowlink[node], lowlink[adj]);
+        }else if(in_s[adj]) {
+            lowlink[node] = min(lowlink[node], tin[adj]);
         }
     }
 
-    if(lowlink[node] == time[node]) {
+    if(lowlink[node] == tin[node]) {
         sccs.push_back({});
         int curr = -1;
         
@@ -34,7 +33,7 @@ void dfs(int node, int &curr_time, vector<vector<int>> &adjacencies, vector<int>
 }
 
 vector<vector<int>> tarjan(vector<vector<int>> &adjacencies) {
-    vector<int> time(adjacencies.size(), -1), lowlink(adjacencies.size(), -1);
+    vector<int> tin(adjacencies.size(), -1), lowlink(adjacencies.size(), -1);
     vector<bool> in_s(adjacencies.size(), false);
     stack<int> s;
     int curr_time = 0;
@@ -42,8 +41,8 @@ vector<vector<int>> tarjan(vector<vector<int>> &adjacencies) {
     vector<vector<int>> sccs;
 
     for(int node = 0; node < adjacencies.size(); node++) {
-        if(time[node] == -1) {
-            dfs(node, curr_time, adjacencies, time, lowlink, s, in_s, sccs);
+        if(tin[node] == -1) {
+            dfs(node, curr_time, adjacencies, tin, lowlink, s, in_s, sccs);
         }
     }
 
