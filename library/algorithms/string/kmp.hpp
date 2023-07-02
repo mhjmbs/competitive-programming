d7484eba8ecaf336d3ccb50e3c26059ee1087114
mhjmbs <mhjmbs@proton.me>
2023-07-02 14:42:23 -0300

    Add KMP Algorithm

---
+++ library/algorithms/string/kmp.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> kmp(const string &s) {
+    vector<int> pi(s.size());
+    pi[0] = 0;
+
+    for(int i = 1, j = 0; i < s.size(); i++) {
+        while(j > 0 && s[i] != s[j]) {
+            j = pi[j-1];
+        }
+        if(s[i] == s[j]) j++;
+        pi[i] = j;
+    }
+
+    return pi;
+}
