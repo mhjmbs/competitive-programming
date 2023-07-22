5622e4b7e974f285face0806f0a7d050fd913423
mhjmbs <mhjmbs@proton.me>
2023-07-22 20:11:57 -0300

    Add Hour for a Run solution

---
+++ solutions/codeforces/gym/2019-2020_acm-icpc_brazil_subregional_programming_contest/hour_for_a_run.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int v, n;
+    cin >> v >> n;
+
+    n *= v;
+    
+    for(int i = 1; i <= 9; i++) {
+        cout << n*i/10 + (n*i%10 != 0 ? 1 : 0) << (i != 9 ? ' ' : '\n'); 
+    }
+}
