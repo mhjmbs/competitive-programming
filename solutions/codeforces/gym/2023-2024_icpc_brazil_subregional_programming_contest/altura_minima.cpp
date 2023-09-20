339ddd49ee16b8e15c0c4b4362e95a945d752284
mhjmbs <mhjmbs@proton.me>
2023-09-20 20:31:28 -0300

    Add Altura Mínima solution

---
+++ solutions/codeforces/gym/2023-2024_icpc_brazil_subregional_programming_contest/altura_minima.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int n, h;
+    cin >> n >> h;
+
+    int ans = 0;
+
+    for(int i = 0, curr; i < n; i++) {
+        cin >> curr;
+        ans += h >= curr ? 1 : 0;
+    }
+
+    cout << ans << '\n';
+}
