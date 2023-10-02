e134abd6bf938fa95f1c741e00036f6dfa857566
mhjmbs <mhjmbs@proton.me>
2023-10-01 21:30:54 -0300

    Add String Task solution

---
+++ solutions/Codeforces/Codeforces Beta Rounds/Codeforces Beta Round 89 (Div. 2)/A - String Task.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool is_vowel(char c) {
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y';
+}
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    for(char c : s) {
+        if(!is_vowel(c)) {
+            cout << '.' << char(tolower(c));
+        }
+    }
+    cout << '\n';
+}
