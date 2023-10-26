0a8a1ddf49f2f7351ecba51803469509f91b46b1
mhjmbs <mhjmbs@proton.me>
2023-10-25 22:28:33 -0300

    Adjust extended euclidean algorithm to work with negative numbers

--- library/math/extended-euclidean-algorithm.hpp
+++ library/math/extended-euclidean-algorithm.hpp
@@ -6,11 +6,18 @@ using namespace std;
 using ll = long long;
 using tlll = tuple<ll,ll,ll>;
 
-tlll egcd(ll a, ll b) {
+tlll pos_egcd(ll a, ll b) {
     if(b == 0) {
         return {a, 1, 0};
     }else {
-        auto [gcd, x, y] = egcd(a, b);
+        auto [gcd, x, y] = pos_egcd(b, a%b);
         return {gcd, y, x-(a/b)*y};
     }
+}
+
+tlll egcd(ll a, ll b) {
+    auto [gcd, x, y] = pos_egcd(abs(a), abs(b));
+    x = (a >= 0) ? x : -x;
+    y = (b >= 0) ? y : -y;
+    return {gcd, x, y};
 }
