3853169065a9aef052355c437aa00225a4ba1f2d
mhjmbs <mhjmbs@proton.me>
2023-12-14 23:49:40 -0300

    Refactor Building Roads solution

--- solutions/CSES/Graph Algorithms/Building Roads.cpp
+++ solutions/CSES/Graph Algorithms/Building Roads.cpp
@@ -1,8 +1,21 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
-int n, m;
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
 vector<vector<int>> adjacencies;
 vector<bool> vis;
 
@@ -17,31 +30,35 @@ void dfs(int node) {
 }
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
+    int n, m;
     cin >> n >> m;
 
-    adjacencies.resize(n+1);
-    vis.resize(n+1);
+    adjacencies.resize(n);
+    vis.resize(n, false);
 
-    for(int i = 0, c1, c2; i < m; i++) {
-        cin >> c1 >> c2;
-        adjacencies[c1].push_back(c2);
-        adjacencies[c2].push_back(c1);
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        adjacencies[u].push_back(v);
+        adjacencies[v].push_back(u);
     }
 
-    vector<int> rep;
+    int cc = 0;
+    vector<int> ans;
 
-    for(int i = 1; i <= n; i++) {
-        if(!vis[i]) {
-            rep.push_back(i);
-            dfs(i);
+    for(int node = 0; node < n; node++) {
+        if(!vis[node]) {
+            ans.push_back(node);
+            cc++;
+            dfs(node);
         }
     }
 
-    cout << rep.size()-1 << '\n';
-
-    for(int i = 0; i < rep.size()-1; i++) {
-        cout << rep[i] << ' ' << rep[i+1] << '\n';
+    cout << cc-1 << '\n';
+    for(int i = 1; i < ans.size(); i++) {
+        cout << ans[i-1]+1 << ' ' << ans[i]+1 << '\n';
     }
 }
