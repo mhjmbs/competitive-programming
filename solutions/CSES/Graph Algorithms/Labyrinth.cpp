ca4ca7fe3f660977001514a8e740c12e37debf6c
mhjmbs <mhjmbs@proton.me>
2023-12-14 22:14:13 -0300

    Refactor Labyrinth solution

--- solutions/CSES/Graph Algorithms/Labyrinth.cpp
+++ solutions/CSES/Graph Algorithms/Labyrinth.cpp
@@ -1,104 +1,106 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int n, m;
+vector<string> grid;
+
+queue<pair<pii,int>> q;
+vector<vector<bool>> enqueued;
+vector<vector<int>> dist;
+vector<vector<char>> lastMove;
+vector<int> di = {0, 1, 0, -1}, dj = {1, 0, -1, 0};
+
+pii getCoord(char c) {
+    for(int i = 0; i < grid.size(); i++) {
+        for(int j = 0; j < grid[i].size(); j++) {
+            if(grid[i][j] == c) return {i,j};
+        }
+    }
+}
+
+bool inbounds(int i, int j) {
+    return (0 <= i && i < n) && (0 <= j && j < m);
+}
 
 int main() {
-    ios::sync_with_stdio(0);
-
-    int n, m;
+    fastio;
 
     cin >> n >> m;
 
-    vector<string> grid(n);
+    grid.resize(n);
+    for(string& line : grid) cin >> line;
 
-    for(string& line : grid) {
-        cin >> line;
-    }
+    pii A = getCoord('A');
+    pii B = getCoord('B');
 
-    pii a, b;
+    enqueued.resize(n, vector<bool>(m, false));
+    dist.resize(n, vector<int>(m, -1));
+    lastMove.resize(n, vector<char>(m));
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(grid[i][j] == 'A') a = make_pair(i,j);
-            if(grid[i][j] == 'B') b = make_pair(i,j);
-        }
-    }
-
-    vector<vector<int>> d(n, vector<int>(m, INT_MAX));
-    vector<vector<bool>> vis(n, vector<bool>(m, false));
-    vector<int> di = {0,1,0,-1}, dj = {1,0,-1,0};
-    vector<vector<char>> lm(n, vector<char>(m, 'x'));
-    queue<pii> q;
-    
-    q.push(a);
-    d[a.first][a.second] = 0;
-    vis[a.first][a.second] = true;
+    dist[A.first][A.second] = 0;
+    q.emplace(A, 0);
+    enqueued[A.first][A.second] = true;
 
     while(!q.empty()) {
-        int i = q.front().first;
-        int j = q.front().second;
+        auto [i,j] = q.front().first;
+        int currDist = q.front().second;
         q.pop();
+        
+        dist[i][j] = currDist;
 
-        if(grid[i][j] == 'B') {
-            break;
-        }
+        for(int k = 0; k < 4; k++) {
+            int ni = i + di[k];
+            int nj = j + dj[k];
 
-        for(int counter = 0; counter < 4; counter++) {
-            int ni = i + di[counter];
-            int nj = j + dj[counter];
-            int cm;
-            switch(counter) {
-                case 0: {
-                    cm = 'R';
-                    break;
-                }
-                case 1: {
-                    cm = 'D';
-                    break;
-                }
-                case 2: {
-                    cm = 'L';
-                    break;
-                }
-                default:
-                    cm = 'U';
-            }
+            if(inbounds(ni, nj) && grid[i][j] != '#' && !enqueued[ni][nj]) {
+                q.emplace(make_pair(ni,nj), currDist+1);
+                enqueued[ni][nj] = true;
 
-            if(0 <= ni && ni < n && 0 <= nj && nj < m &&
-               grid[ni][nj] != '#' && !vis[ni][nj]) {
-                q.emplace(ni,nj);
-                vis[ni][nj] = true;
-                lm[ni][nj] = cm;
+                if(k == 0) lastMove[ni][nj] = 'R';
+                else if(k == 1) lastMove[ni][nj] = 'D';
+                else if(k == 2) lastMove[ni][nj] = 'L';
+                else  lastMove[ni][nj] = 'U';
             }
         }
     }
 
-    if(lm[b.first][b.second] == 'x') {
+    if(dist[B.first][B.second] == -1) {
         cout << "NO\n";
         return 0;
     }
 
     cout << "YES\n";
+    cout << dist[B.first][B.second] << '\n';
 
-    int i = b.first;
-    int j = b.second;
+    string path;
+    pii curr = B;
 
-    stack<char> path;
-    while(i != a.first || j != a.second) {
-        char clm = lm[i][j];
-        path.push(clm);
+    while(curr != A) {
+        char move = lastMove[curr.first][curr.second];
+        path.push_back(move);
 
-        if(clm == 'R') j--;
-        else if(clm == 'D') i--;
-        else if(clm == 'L') j++;
-        else i++;
+        if(move == 'R') curr.second--;
+        else if(move == 'D') curr.first--;
+        else if(move == 'L') curr.second++;
+        else if(move == 'U') curr.first++;
     }
+
+    reverse(path.begin(), path.end());
     
-    cout << path.size() << '\n';
-    while(!path.empty()) {
-        cout << path.top();
-        path.pop();
-    }
-    cout << '\n';
+    cout << path << '\n';
 }
