106ba36be33a56b2fc85b33bd63bb66e83ab3cf1
mhjmbs <mhjmbs@proton.me>
2023-12-17 12:53:43 -0300

    Refactor Segment Tree

--- library/data-structures/segment-tree.hpp
+++ library/data-structures/segment-tree.hpp
@@ -4,49 +4,61 @@
 
 using namespace std;
 
-using ll = long long;
-
+template <typename T>
 class SegmentTree {
-    vector<ll> seg;
-    int lr_start;
-    int lr_size = 1;
+    vector<T> seg;
+    int leafsBegin;
+    int leafsCount;
+
+    T op(T a, T b) {
+        return a + b;
+    }
+
+    T neutral() {
+        return 0;
+    }
 
     public:
 
-    SegmentTree(vector<int> vec) {
-        while(lr_size < vec.size()) {
-            lr_size *= 2;
+    SegmentTree(const vector<T>& vec) {
+        leafsCount = 1;
+
+        while(leafsCount < vec.size()) {
+            leafsCount *= 2;
         }
 
-        seg.resize(2*lr_size-1);
+        seg.resize(2*leafsCount-1, neutral());
         
-        lr_start = seg.size() - lr_size;
+        leafsBegin = seg.size() - leafsCount;
 
         for(int i = 0; i < vec.size(); i++) {
-            seg[lr_start + i] = vec[i];
+            seg[leafsBegin + i] = vec[i];
         }
 
-        for(int i = lr_start - 1; 0 <= i; i--) {
-            seg[i] = seg[2*i+1] + seg[2*i+2];
+        for(int i = leafsBegin - 1; i >= 0; i--) {
+            seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
     }
 
     void update(int i, int val) {
-        i = lr_start + i;
+        i = leafsBegin + i;
         seg[i] = val;
 
         while(i != 0) {
             i = (i-1)/2;
-            seg[i] = seg[2*i+1] + seg[2*i+2];
+            seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
     }
 
-    ll query(int l, int r, int lx = 0, int rx = -1, int i = 0) {
-        if(rx == -1) rx = lr_size-1;
+    T query(int l, int r) {
+        return query(l, r, 0, leafsCount-1, 0);
+    }
+
+    T query(int l, int r, int lx, int rx, int i) {
         int mid = (lx+rx)/2;
 
         if(l <= lx && rx <= r) return seg[i];
-        else if(rx < l|| r < lx) return 0;
-        else return query(l, r, lx, mid, 2*i+1) + query(l, r, mid+1, rx, 2*i+2);
+        else if(rx < l|| r < lx) return neutral();
+        else return op( query(l, r, lx, mid, 2*i+1), query(l, r, mid+1, rx, 2*i+2) );
     }
 };
