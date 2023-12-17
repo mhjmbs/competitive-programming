b839c0bf2ee8f50815a54510ce83af156c7abcf5
mhjmbs <mhjmbs@proton.me>
2023-12-17 13:41:08 -0300

    Refactor Round Trip solution

--- solutions/CSES/Graph Algorithms/Round Trip.cpp
+++ solutions/CSES/Graph Algorithms/Round Trip.cpp
@@ -1,64 +1,79 @@
-#include "bits/stdc++.h"
-
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+ 
 using namespace std;
+using namespace __gnu_pbds;
+ 
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+ 
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+ 
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+vector<vector<int>> adjacencies;
+vector<bool> vis;
+vector<int> last;
+ 
+void printCycle(int cycleEnd, int cycleBegin) {
+    vector<int> cycle;
 
-    int n, m;
-    bool printed = false;
-    vector<bool> vis;
-    vector<vector<int>> adjacencies;
-    stack<int> s;
-
-    void dfs(int node, int father) {
-        vis[node] = true;
-        s.push(node);
-
-        for(int adj : adjacencies[node]) {
-            if(adj == father) continue;
-            if(!vis[adj]) {
-                dfs(adj, node);
-            }else {
-                if(!printed) {
-                    printed = true;
-                    
-                    vector<int> path = {adj};
-                    int v;
-
-                    do {
-                        v = s.top();
-                        s.pop();
-                        path.push_back(v);
-                    }while(v != adj);
-
-                    cout << path.size() << '\n';
-                    for(int i = 0; i < path.size(); i++) {
-                        cout << path[i];
-                        if(i+1 == path.size()) cout << '\n';
-                        else cout << ' ';
-                    }
-                }
-            }
-        }
-
-        if(s.size() > 0) s.pop();
+    int node = cycleEnd;
+    while(node != cycleBegin) {
+        cycle.push_back(node);
+        node = last[node];
     }
+    cycle.push_back(cycleBegin);
+    cycle.push_back(cycleEnd);
 
+    cout << cycle.size() << '\n';
+ 
+    for(int i = 0; i < cycle.size(); i++) {
+        cout << cycle[i] << (i != cycle.size()-1 ? ' ' : '\n');
+    }
+}
+ 
+void dfs(int node, int previous) {
+    vis[node] = true;
+    last[node] = previous;
+ 
+    for(int adj : adjacencies[node]) {
+        if(!vis[adj]) {
+            dfs(adj, node);
+        }else if(previous != adj) {
+            printCycle(node, adj);
+            exit(0);
+        }
+    }
+}
+ 
 int main() {
-    ios::sync_with_stdio(0);
-    
+    fastio;
+ 
+    int n, m;
     cin >> n >> m;
-    
+ 
     adjacencies.resize(n+1);
     vis.resize(n+1, false);
-
-    for(int i = 0, u, v; i < m; i++) {
+    last.resize(n+1, -1);
+ 
+    for(int i = 0; i < m; i++) {
+        int u, v;
         cin >> u >> v;
         adjacencies[u].push_back(v);
         adjacencies[v].push_back(u);
     }
-
-    for(int i = 1; i <= n; i++) {
-        if(!vis[i]) dfs(i, 0);
+ 
+    for(int node = 1; node <= n; node++) {
+        if(!vis[node]) {
+            dfs(node, -1);
+        }
     }
-
-    if(!printed) cout << "IMPOSSIBLE\n";
+ 
+    cout << "IMPOSSIBLE\n";
 }
