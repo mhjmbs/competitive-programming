fb5350340d421baf67e4c47c271f6c8a81adfde2
mhjmbs <mhjmbs@proton.me>
2024-01-18 21:58:40 -0300

    Add CTZ solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 336/B - CTZ.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+
+    int pow = 1;
+    int ans = 0;
+
+    for(int count = 0; count < 32; count++) {
+        if(n & pow) {
+            break;
+        }else {
+            ans++;
+            pow *= 2;
+        }
+    }
+
+    cout << ans << '\n';
+}
