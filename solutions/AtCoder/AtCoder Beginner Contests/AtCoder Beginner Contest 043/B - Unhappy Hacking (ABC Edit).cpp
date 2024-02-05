4edaea26c509010eb34b363ea28884b7408a0b7b
mhjmbs <mhjmbs@proton.me>
2024-02-05 17:02:14 -0300

    Add Unhappy Hacking (ABC Edit) solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 043/B - Unhappy Hacking (ABC Edit).cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s, ans;
+    cin >> s;
+
+    for(char c : s) {
+        if(c == '0') ans.push_back('0');
+        else if(c == '1') ans.push_back('1');
+        else if(c == 'B' && !ans.empty()) ans.pop_back();
+    }
+
+    cout << ans << '\n';
+}
