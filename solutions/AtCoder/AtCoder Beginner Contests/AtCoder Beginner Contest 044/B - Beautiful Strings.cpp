9e31a7db07401aa9879d52629c741d9624955164
mhjmbs <mhjmbs@proton.me>
2024-02-05 21:13:32 -0300

    Add Beautiful Strings solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 044/B - Beautiful Strings.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string w;
+    cin >> w;
+
+    vector<int> freq(26 ,0);
+    for(char wi : w) freq[wi-'a']++;
+
+    cout << (all_of(freq.begin(), freq.end(), [](int x){return x%2 == 0;})? "Yes\n" : "No\n");
+}
