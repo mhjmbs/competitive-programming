c61fcedad9445268eae1a123428e4658acf38c55
mhjmbs <mhjmbs@proton.me>
2024-02-09 17:25:08 -0300

    Refactor Segment Tree

--- library/data-structures/segment-tree.hpp
+++ library/data-structures/segment-tree.hpp
@@ -5,46 +5,32 @@
 using namespace std;
 
 template <typename T>
-class SegmentTree {
+struct SegmentTree {
+    inline static const T neutral = numeric_limits<T>::max();
     vector<T> seg;
     int leafsBegin;
     int leafsCount;
 
-    T op(T a, T b) {
-        return a + b;
-    }
-
-    T neutral() {
-        return 0;
-    }
-
-    public:
-
-    SegmentTree(const vector<T>& vec) {
+    SegmentTree(const vector<T>& v) {
         leafsCount = 1;
-
-        while(leafsCount < vec.size()) {
-            leafsCount *= 2;
-        }
-
-        seg.resize(2*leafsCount-1, neutral());
+        while(leafsCount < v.size()) leafsCount *= 2;
+        seg.resize(2*leafsCount-1, neutral);
         
         leafsBegin = seg.size() - leafsCount;
 
-        for(int i = 0; i < vec.size(); i++) {
-            seg[leafsBegin + i] = vec[i];
+        for(int i = 0; i < v.size(); i++) {
+            seg[leafsBegin+i] = v[i];
         }
 
-        for(int i = leafsBegin - 1; i >= 0; i--) {
+        for(int i = leafsBegin-1; i >= 0; i--) {
             seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
     }
 
-    void update(int i, int val) {
+    void upd(int i, T val) {
         i = leafsBegin + i;
         seg[i] = val;
-
-        while(i != 0) {
+        while(i > 0) {
             i = (i-1)/2;
             seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
@@ -55,10 +41,13 @@ class SegmentTree {
     }
 
     T query(int l, int r, int lx, int rx, int i) {
-        int mid = (lx+rx)/2;
-
+        if(rx < l || r < lx) return neutral;
         if(l <= lx && rx <= r) return seg[i];
-        else if(rx < l|| r < lx) return neutral();
-        else return op( query(l, r, lx, mid, 2*i+1), query(l, r, mid+1, rx, 2*i+2) );
+        int mid = (lx+rx)/2;
+        return op( query(l, r, lx, mid, 2*i+1), query(l, r, mid+1, rx, 2*i+2) );
+    }
+
+    T op(T a, T b) {
+        return min(a,b);
     }
 };
