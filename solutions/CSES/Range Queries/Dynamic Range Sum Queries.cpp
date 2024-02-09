e73d5edffcd37703bb8074512963b13f3754f876
mhjmbs <mhjmbs@proton.me>
2024-02-09 14:27:53 -0300

    Refactor Dynamic Range Sum Queries solution

--- solutions/CSES/Range Queries/Dynamic Range Sum Queries.cpp
+++ solutions/CSES/Range Queries/Dynamic Range Sum Queries.cpp
@@ -1,55 +1,47 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
 
-struct SegTree {
-    vector<ll> seg;
-    int lr_size = 1;
-    int lr_start;
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
 
-    SegTree(vector<int> x) {
-        setSize(x.size());
+template <typename T>
+struct FenwickTree {
+    int n;
+    vector<T> tree;
 
-        lr_start = seg.size() - lr_size;
-
-        for(int i = 0; i < lr_size; i++) {
-            seg[lr_start + i] = x[i];
-        }
-
-        for(int i = lr_start-1; 0 <= i; i--) {
-            seg[i] = seg[2*i+1] + seg[2*i+2];
-        }
-    }
-
-    void setSize(int array_size) {
-        while(lr_size < array_size) lr_size *= 2;
-        seg.resize(2*lr_size-1, 0);
-    }
-
-    void set(int k, int u) {
-        int i = lr_start + k;
-        seg[i] = u;
-
-        while(i > 0) {
-            if(i % 2 == 0) i = (i-2)/2;
-            else i = (i-1)/2;
-
-            seg[i] = seg[2*i+1] + seg[2*i+2];
+    FenwickTree(vector<T>& v) : n(v.size()), tree(n, 0) {
+        for(int i = 1; i <= n; i++) {
+            tree[i] += v[i-1];
+            if(i+(i&-i) <= n) tree[i+(i&-i)] += tree[i];
         }
     }
 
-    ll query(int l, int r, int lx = 0, int rx = -1, int i = 0) {
-        if(rx == -1) rx = lr_size-1;
+    void sum(int i, T val) {
+        while(i <= n) {
+            tree[i] += val;
+            i += i&-i;
+        }
+    }
 
-        int m = (lx+rx)/2;
-
-        if(rx < l || r < lx) return 0;
-        if(l <= lx && rx <= r) return seg[i];
-        return query(l, r, lx, m, 2*i+1) + query(l, r, m+1, rx, 2*i+2);
+    T query(int r) {
+        T ans = 0;
+        while(r > 0) {
+            ans += tree[r];
+            r -= r&-r;
+        }
+        return ans;
     }
 };
 
@@ -59,23 +51,20 @@ int main() {
     int n, q;
     cin >> n >> q;
 
-    vector<int> x(n);
-    for(int &num : x) cin >> num;
-    
-    SegTree seg(x);
+    vector<ll> x(n);
+    for(ll&xi : x) cin >> xi;
 
-    for(int i = 0, tq; i < q; i++) {
-        cin >> tq;
-        if(tq == 1){
-            int k, u;
-            cin >> k >> u;
-            k--;
-            seg.set(k, u);
+    FenwickTree<ll> ft(x);
+
+    while(q--) {
+        int t, a, b;
+        cin >> t >> a >> b;
+
+        if(t == 1) {
+            ft.sum(a, b-x[a-1]);
+            x[a-1] = b;
         }else {
-            int l, r;
-            cin >> l >> r;
-            l--;r--;
-            cout << seg.query(l, r) << '\n';
+            cout << ft.query(b)-ft.query(a-1) << '\n';
         }
     }
 }
