7afdf7560c598c6055de4afe7a69831b87224e85
mhjmbs <mhjmbs@proton.me>
2024-02-09 15:46:22 -0300

    Add 1D Reversi solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 047/C - 1D Reversi.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    int ans = 0;
+
+    for(int i = 1; i < s.size(); i++) {
+        if(s[i] != s[i-1]) ans++;
+    }
+
+    cout << ans << '\n';
+}
