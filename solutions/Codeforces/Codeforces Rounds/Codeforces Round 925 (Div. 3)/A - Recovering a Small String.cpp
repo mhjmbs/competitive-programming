7ec83815ebb1e374f98c3d2e216bd13fc9ccf152
mhjmbs <mhjmbs@proton.me>
2024-02-14 21:16:03 -0300

    Add Recovering a Small String solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 925 (Div. 3)/A - Recovering a Small String.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        string ans = "aaa";
+
+        n -= 3;
+
+        while(n > 0) {
+            if(ans[2] != 'z') ans[2]++;
+            else if(ans[1] != 'z') ans[1]++;
+            else ans[0]++;
+            n--;
+        }
+
+        cout << ans << '\n';
+    }
+}
