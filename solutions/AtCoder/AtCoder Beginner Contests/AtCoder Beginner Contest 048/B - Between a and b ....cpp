3575da7038550d9a81d5d2d1ff2527470bfea1d4
mhjmbs <mhjmbs@proton.me>
2024-02-15 01:45:27 -0300

    Add Between a and b ... solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 048/B - Between a and b ....cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    ll a, b, x;
+    cin >> a >> b >> x;
+
+    cout << b/x+1 - (a == 0 ? 0 : (a-1)/x+1) << '\n';
+}
