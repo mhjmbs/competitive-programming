d41d6cf60ebdcb10be7be5e2fbb0fb349b8d97ef
mhjmbs <mhjmbs@proton.me>
2024-02-15 01:45:54 -0300

    Add An Ordinary Game solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 048/D - An Ordinary Game.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    bool second = (s.front() == s.back() && s.size() % 2 == 1) ||  (s.front() != s.back() && s.size() % 2 == 0);
+
+    cout << (second ? "Second\n" : "First\n");
+}
