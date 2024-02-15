220c4b1868adf0050cdebcd5fc86f54f9b6ae317
mhjmbs <mhjmbs@proton.me>
2024-02-15 17:05:05 -0300

    Add Rectangle Cutting solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 924 (Div. 2)/A - Rectangle Cutting.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        ll a, b;
+        cin >> a >> b;
+        if(b > a) swap(a,b);
+
+        bool yes = false;
+
+        if(b%2 == 0 && 2*a != b || (a%2 == 0) && a != 2*b) yes = true;
+
+        cout << (yes ? "Yes\n" : "No\n");
+    }
+}
