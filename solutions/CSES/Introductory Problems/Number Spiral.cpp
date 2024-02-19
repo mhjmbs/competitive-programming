3b2376b0986836d34269a5322962506e47c5e26a
mhjmbs <mhjmbs@proton.me>
2024-02-19 10:48:54 -0300

    Refactor Number Spiral solution

--- solutions/CSES/Introductory Problems/Number Spiral.cpp
+++ solutions/CSES/Introductory Problems/Number Spiral.cpp
@@ -1,28 +1,47 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
 using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     int t;
     cin >> t;
 
     while(t--) {
-        ll y, x, ans;
+        int x, y;
         cin >> y >> x;
 
-        if(y >= x) {
-            ans = 1 + (y-1)*y;
-            if(y % 2 == 0) ans += y-x;
-            else ans -= y-x;
+        ll sq = max(x,y);
+        ll curr = sq*sq;
+
+        if(sq % 2 == 1) {
+            int x2 = sq, y2 = 1;
+            curr -= y - y2;
+            curr -= x2 - x;
         }else {
-            ans = 1 + (x-1)*x;
-            if(x % 2 == 0) ans -= x-y;
-            else ans += x-y;
+            int x2 = 1, y2 = sq;
+            curr -= x - x2;
+            curr -= y2 - y;
         }
 
-        cout << ans << '\n'; 
-    }    
+        cout << curr << '\n';
+    }
 }
