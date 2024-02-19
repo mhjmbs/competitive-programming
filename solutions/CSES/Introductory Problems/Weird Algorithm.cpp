85812531b9a8e1ef5ed36ce90e18617ad895249c
mhjmbs <mhjmbs@proton.me>
2024-02-19 10:03:21 -0300

    Refactor Weird Algorithm solution

--- solutions/CSES/Introductory Problems/Weird Algorithm.cpp
+++ solutions/CSES/Introductory Problems/Weird Algorithm.cpp
@@ -1,10 +1,26 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
 using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     ll n;
     cin >> n;
@@ -12,7 +28,8 @@ int main() {
     while(n != 1) {
         cout << n << ' ';
         if(n % 2 == 0) n /= 2;
-        else n = 3*n + 1;
+        else n = 3*n+1;
     }
-    cout << n << '\n';
+
+    cout << 1 << '\n';
 }
