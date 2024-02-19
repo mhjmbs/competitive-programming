62cde0ae79b1a2498074d4bc711dc483109580b2
mhjmbs <mhjmbs@proton.me>
2024-02-19 10:15:02 -0300

    Refactor Increasing Array solution

--- solutions/CSES/Introductory Problems/Increasing Array.cpp
+++ solutions/CSES/Introductory Problems/Increasing Array.cpp
@@ -1,20 +1,40 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
 using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
-    int n, last = 0;
+    int n;
     cin >> n;
-    
+
+    vector<int> a(n);
+    for(int& ai : a) cin >> ai;
+
     ll ans = 0;
 
-    for(int i = 0, inp; i < n; i++) {
-        cin >> inp;
-        ans += max(0, last-inp);
-        last = max(inp, last);
+    for(int i = 1; i < n; i++) {
+        if(a[i-1] > a[i]) {
+            ans += a[i-1] - a[i];
+            a[i] = a[i-1];
+        }
     }
 
     cout << ans << '\n';
