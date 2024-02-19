d99f28b00eb790e448b54374e47fb36e7850aea4
mhjmbs <mhjmbs@proton.me>
2024-02-19 10:24:14 -0300

    Refactor Permutations solution

--- solutions/CSES/Introductory Problems/Permutations.cpp
+++ solutions/CSES/Introductory Problems/Permutations.cpp
@@ -1,33 +1,41 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     int n;
     cin >> n;
 
-    if(n <= 3) {
-        if(n == 1) cout << "1\n";
-        else cout << "NO SOLUTION\n";
+    if(n == 2 || n == 3) {
+        cout << "NO SOLUTION\n";
         return 0;
     }
 
-    deque<int> dq = {2,4,1,3};
+    vector<int> p;
+    p.reserve(n);
 
-    bool left = true;
-    for(int i = 5; i <= n; i++) {
-        if(left) dq.push_front(i);
-        else dq.push_back(i);
-        left = !left;
-    }
+    for(int i = 2; i <= n; i += 2) p.push_back(i);
+    for(int i = 1; i <= n; i += 2) p.push_back(i);
 
-    while(!dq.empty()) {
-        cout << dq.front();
-        dq.pop_front();
-        if(dq.size() == 0) cout << '\n';
-        else cout << ' ';
-    }
-    
+    for(int pi : p) cout << pi << ' ';
+    cout << '\n';
 }
