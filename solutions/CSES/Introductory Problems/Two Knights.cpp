bbbd47c741d9b4f9eaa8387a53ea1faba006ae5c
mhjmbs <mhjmbs@proton.me>
2024-02-19 11:33:57 -0300

    Refactor Two Knights solution

--- solutions/CSES/Introductory Problems/Two Knights.cpp
+++ solutions/CSES/Introductory Problems/Two Knights.cpp
@@ -1,17 +1,31 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
 using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
-    int n;
+    ll n;
     cin >> n;
 
-    for(ll k = 1; k <= n; k++) {
-        ll ans = (k*k * (k*k-1))/2;
-        ans -= 4*max(k-2,0ll)*max(k-1,0ll);
-        cout << ans << '\n';
+    for(__uint128_t i = 1; i <= n; i++) {
+        cout << ull( (i*i*(i*i-1)/2) - (8*i*i - 8*(3*i-2))/2 ) << '\n';
     }
 }
