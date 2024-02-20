1be18fa2456b7b0112051d2bb1a000963e6e6a1c
mhjmbs <mhjmbs@proton.me>
2024-02-19 23:38:30 -0300

    Add Vlad and the Best of Five solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 928 (Div. 4)/A - Vlad and the Best of Five.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        string s;
+        cin >> s;
+    
+        int a = 0, b = 0;
+
+        for(char c : s) {
+            if(c == 'A') a++;
+            else b++;
+        }
+
+        if(a >= b) cout << "A\n";
+        else cout << "B\n";
+    }
+}
