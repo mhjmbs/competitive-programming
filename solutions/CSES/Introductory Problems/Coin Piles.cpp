69bd83c4abfe7eeea0ef0408518c0bf1cb97ba71
mhjmbs <mhjmbs@proton.me>
2024-02-21 19:36:12 -0300

    Refactor Coin Piles solution

--- solutions/CSES/Introductory Problems/Coin Piles.cpp
+++ solutions/CSES/Introductory Problems/Coin Piles.cpp
@@ -1,12 +1,27 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
 using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+bool hasSolution(int a, int b) {
+    if(2*a-b < 0 || (2*a-b) % 3 != 0) return false;
+    if(a < 2*(2*a-b) /3) return false;
+    return true;
+}
 
 int main() {
     fastio;
@@ -15,15 +30,8 @@ int main() {
     cin >> t;
 
     while(t--) {
-        ll a, b;
+        int a, b;
         cin >> a >> b;
-
-        bool poss1 = true, poss2 = true;
-
-        if((a-2*b) % -3 != 0 || (a-2*b) / -3 < 0 || b - 2*(a-2*b) / -3 < 0) poss1 = false;
-        if((b-2*a) % -3 != 0 || (b-2*a) / -3 < 0 || a - 2*(b-2*a) / -3 < 0) poss2 = false;
-
-        if(poss1 || poss2) cout << "YES\n";
-        else cout << "NO\n";
+        cout << (hasSolution(a,b) || hasSolution(b,a) ? "YES\n" : "NO\n");
     }
 }
