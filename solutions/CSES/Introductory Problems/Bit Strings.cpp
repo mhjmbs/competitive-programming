625cc7f493f617af9ab878ec84b4b816a437ecff
mhjmbs <mhjmbs@proton.me>
2024-02-21 18:53:33 -0300

    Refactor Bit Strings solution

--- solutions/CSES/Introductory Problems/Bit Strings.cpp
+++ solutions/CSES/Introductory Problems/Bit Strings.cpp
@@ -1,19 +1,34 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     int n;
     cin >> n;
-    int m = int(1e9) + 7;
 
-    int ans = 1;
+    ll ans = 1;
+    ll m = 1e9+7;
 
-    for(int i = 0; i < n; i++) {
-        ans *= 2;
-        ans %= m;
-    }
+    for(int i = 0; i < n; i++) ans = ans * 2 % m;
+
     cout << ans << '\n';
 }
