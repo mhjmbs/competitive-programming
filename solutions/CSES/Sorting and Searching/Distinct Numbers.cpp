66e389793c553fb241dd0f867bd6f1f1ab5ce922
mhjmbs <mhjmbs@proton.me>
2024-02-22 21:41:27 -0300

    Refactor Distinct Numbers solution

--- solutions/CSES/Sorting and Searching/Distinct Numbers.cpp
+++ solutions/CSES/Sorting and Searching/Distinct Numbers.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,12 +25,12 @@ int main() {
     int n;
     cin >> n;
 
-    set<int> distinct;
-
-    for(int i = 0, inp; i < n; i++) {
-        cin >> inp;
-        distinct.insert(inp);
+    set<int> x;
+    while(n--) {
+        int xi;
+        cin >> xi;
+        x.insert(xi);
     }
 
-    cout << distinct.size() << '\n';
+    cout << x.size() << '\n';
 }
