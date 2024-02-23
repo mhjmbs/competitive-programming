d57bb0e1792c25ff42afe0c9a6d5b04ced520827
mhjmbs <mhjmbs@proton.me>
2024-02-22 23:07:50 -0300

    Refactor Sum of Two Values solution

--- solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
+++ solutions/CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,39 +25,22 @@ int main() {
     int n, x;
     cin >> n >> x;
 
-    vector<ll> a(n);
-    for(ll &x : a) cin >> x;
-    vector<ll> original = a;
+    vector<pii> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i].first;
+        a[i].second = i+1;
+    }
+
     sort(a.begin(), a.end());
 
-    pii ans = {-1,-1};
-
-    for(int i = 0; i < n-1; i++) {
-        ll target = x - a[i]; 
-        auto it = lower_bound(a.begin()+i+1, a.end(), target);
-        if(it != a.end() && *it == target) {
-            ans = {a[i], *it};
-            break;
+    for(int i = 0; i < n; i++) {
+        auto [curr, pos] = a[i];
+        auto it = lower_bound(a.begin()+i+1, a.end(), make_pair(x-curr,0));
+        if(it != a.end() && it->first == x-curr) {
+            cout << pos << ' ' << it->second << '\n';
+            return 0;
         }
     }
 
-    if(ans != make_pair(-1,-1)) {
-        for(int i = 0; i < n; i++) {
-            if(original[i] == ans.first) {
-                ans.first = i+1;
-                break;
-            }
-        }
-
-        for(int i = n-1; 0 <= i; i--) {
-            if(original[i] == ans.second) {
-                ans.second = i+1;
-                break;
-            }
-        }
-
-        cout << ans.first << ' ' << ans.second << '\n';
-    }else {
-        cout << "IMPOSSIBLE\n";
-    }
+    cout << "IMPOSSIBLE\n";
 }
