1b38de8163db1efc5c1375247761e1b99a5f40a3
mhjmbs <mhjmbs@proton.me>
2024-02-22 22:41:13 -0300

    Refactor Restaurant Customers solution

--- solutions/CSES/Sorting and Searching/Restaurant Customers.cpp
+++ solutions/CSES/Sorting and Searching/Restaurant Customers.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,24 +25,27 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pii> times(2*n);
+    priority_queue<pii, vector<pii>, greater<pii>> prioq;
 
-    for(int i = 0, a, b; i < 2*n; i+=2) {
+    for(int i = 0; i < n; i++) {
+        int a, b;
         cin >> a >> b;
-        times[i] = {a, 0};
-        times[i+1] = {b, 1};
+        prioq.emplace(a,1);
+        prioq.emplace(b,-1);
     }
 
-    sort(times.begin(), times.end());
+    int ans = 0;
+    int in = 0;
 
-    int ans = 0, now = 0;
+    while(!prioq.empty()) {
+        auto [curr, event] = prioq.top();
+    
+        while(!prioq.empty() && prioq.top().first == curr) {
+            in += prioq.top().second;
+            prioq.pop();
+        }
 
-    for(pii customer : times) {
-        int status = customer.second;
-        
-        if(status == 0) now++;
-        else now--;
-        ans = max(ans, now);
+        ans = max(ans, in);
     }
 
     cout << ans << '\n';
