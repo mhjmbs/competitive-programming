33685f11f1c48037af147b0679a3184bbaa738af
mhjmbs <mhjmbs@proton.me>
2024-02-23 17:07:13 -0300

    Add Moving Chips solution

---
+++ solutions/Codeforces/Educational Codeforces Rounds/Educational Codeforces Round 162 (Rated for Div. 2)/A - Moving Chips.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int& ai : a) cin >> ai;
+
+        int r = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(a[i] == 1) r = i;
+        }
+
+        int in = 0;
+        ll ans = 0;
+
+        for(int i = r-1; i >= 0; i--) {
+            if(a[i] == 0) in++;
+            if(a[i] == 1 && in) {
+                ans += in;
+                in = 0;
+            }
+        }
+
+        cout << ans << '\n';
+    }
+}
