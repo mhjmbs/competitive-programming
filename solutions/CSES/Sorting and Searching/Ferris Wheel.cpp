9a9ee2f2bb3b91211ff49f9b6b320fec86352cf2
mhjmbs <mhjmbs@proton.me>
2024-02-22 22:16:17 -0300

    Refactor Ferris Wheel solution

--- solutions/CSES/Sorting and Searching/Ferris Wheel.cpp
+++ solutions/CSES/Sorting and Searching/Ferris Wheel.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,28 +25,27 @@ int main() {
     int n, x;
     cin >> n >> x;
 
-    int gond = 0;
+    multiset<int> p;
+    for(int i = 0; i < n; i++) {
+        int pi;
+        cin >> pi;
+        p.insert(pi);
+    }
 
-    vector<int> p(n);
-    for(int &x : p) cin >> x;
-    sort(p.begin(), p.end());
+    int ans = 0;
 
-    vector<bool> used(n, false);
+    while(!p.empty()) {
+        int curr = *p.begin();
+        p.erase(p.begin());
 
-    int l = 0, r = n-1;
+        ans++;
 
-    while(l < r) {
-        if(p[l] + p[r] <= x) {
-            used[l] = used[r] = true;
-            gond++; l++; r--;
-        }else {
-            r--;
+        auto it = p.upper_bound(x-curr);
+        if(it != p.begin()) {
+            it--;
+            p.erase(it);
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        if(!used[i]) gond++;
-    }
-
-    cout << gond << '\n';
+    cout << ans << '\n';
 }
