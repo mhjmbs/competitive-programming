feb16a2aa241465a0e5a800a3e3cbe8e5fc5f772
mhjmbs <mhjmbs@proton.me>
2024-02-22 23:54:46 -0300

    Refactor Collecting Numbers solution

--- solutions/CSES/Sorting and Searching/Collecting Numbers.cpp
+++ solutions/CSES/Sorting and Searching/Collecting Numbers.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,21 +25,17 @@ int main() {
     int n;
     cin >> n;
 
-    vector<int> a(n);
-    for(int &x : a) cin >> x;
-    set<int> s;
+    vector<int> x(n);
+    for(int& xi : x) cin >> xi;
 
-    int ans = 0;
+    set<int> rounds;
 
-    for(int x : a) {
-        auto it = s.find(x-1);
-        if(it != s.end()) {
-            s.erase(it);
-            ans--;
+    for(int xi : x) {
+        if(rounds.count(xi-1) > 0) {
+            rounds.erase(xi-1);
         }
-        ans++;
-        s.insert(x);
+        rounds.insert(xi);
     }
 
-    cout << s.size() << '\n';
+    cout << rounds.size() << '\n';
 }
