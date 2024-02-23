dbacd6231e6107aa7ed16b80380e3e1ef4577ff9
mhjmbs <mhjmbs@proton.me>
2024-02-22 21:55:03 -0300

    Refactor Apartments solution

--- solutions/CSES/Sorting and Searching/Apartments.cpp
+++ solutions/CSES/Sorting and Searching/Apartments.cpp
@@ -1,38 +1,49 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
 
-    int n, m, k;
+    ll n, m, k;
     cin >> n >> m >> k;
 
-    vector<int> a(n);
-
-    for(int &x : a) cin >> x;
-
-    vector<int> b(m);
-
-    for(int &x : b) cin >> x;
+    vector<ll> a(n), b(m);
+    for(ll& ai : a) cin >> ai;
+    for(ll& bi : b) cin >> bi;
 
     sort(a.begin(), a.end());
     sort(b.begin(), b.end());
 
+    int i = 0, j = 0;
     int ans = 0;
 
-    for(int i = 0, j = 0; i < m; i++) {
-        while(j < n) {
-            if(a[j] - k <= b[i] && b[i] <= a[j]+k) {
-                ans++;
-                j++;
-                break;
-            }else if(b[i] < a[j]+k) break;
+    while(i < n && j < m) {
+        if(a[i]-k <= b[j] && b[j] <= a[i]+k) {
+            ans++;
+            i++;
             j++;
+        }else if(b[j] < a[i]-k) {
+            j++;
+        }else {
+            i++;
         }
     }
 
