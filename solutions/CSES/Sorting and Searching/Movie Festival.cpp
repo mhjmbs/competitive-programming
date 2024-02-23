c214b559e4bf309f2e44f85231e53d145ce8e2b2
mhjmbs <mhjmbs@proton.me>
2024-02-22 22:50:25 -0300

    Refactor Movie Festival solution

--- solutions/CSES/Sorting and Searching/Movie Festival.cpp
+++ solutions/CSES/Sorting and Searching/Movie Festival.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,22 +25,19 @@ int main() {
     int n;
     cin >> n;
 
-    vector<pii> times(n);
+    vector<pii> intervals(n);
+    for(auto& [l,r] : intervals) cin >> l >> r;
 
-    for(auto &x : times) cin >> x.first >> x.second;
-
-    auto comp = [&](pii a, pii b) {
+    sort(intervals.begin(), intervals.end(),
+    [](pii a, pii b) {
         return a.second < b.second;
-    };
+    });
 
-    sort(times.begin(), times.end(), comp);
+    int curr = 0, ans = 0;
 
-    int ans = 0;
-    int now = 0;
-
-    for(pii time : times) {
-        if(now <= time.first) {
-            now = time.second;
+    for(auto [l,r] : intervals) {
+        if(curr <= l) {
+            curr = r;
             ans++;
         }
     }
