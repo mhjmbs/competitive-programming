540a9e6cec651a6f9495e274e16b6bb10fc7d437
mhjmbs <mhjmbs@proton.me>
2024-02-22 23:32:31 -0300

    Refactor Stick Lengths solution

--- solutions/CSES/Sorting and Searching/Stick Lengths.cpp
+++ solutions/CSES/Sorting and Searching/Stick Lengths.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -13,13 +26,14 @@ int main() {
     cin >> n;
 
     vector<int> p(n);
-    for(int &x : p) cin >> x;
+    for(int& pi : p) cin >> pi;
+
     sort(p.begin(), p.end());
 
-    int target = p[p.size()/2];
-
+    int x = p[n/2];
     ll ans = 0;
-    for(int x : p) ans += abs(x - target);
+
+    for(int pi : p) ans += abs(x-pi);
 
     cout << ans << '\n';
 }
