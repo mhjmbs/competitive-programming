1582da34500c960b65c320ad8d5edd56473e81e1
mhjmbs <mhjmbs@proton.me>
2024-02-24 16:55:17 -0300

    Add Which is ahead? solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 342/B - Which is ahead?.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+
+    vector<int> pos(n+1);
+    for(int i = 1; i <= n; i++) {
+        int pi;
+        cin >> pi;
+        pos[pi] = i;
+    }
+
+    int q;
+    cin >> q;
+
+    while(q--) {
+        int ai, bi;
+        cin >> ai >> bi;
+
+        if(pos[ai] < pos[bi]) cout << ai << '\n';
+        else cout << bi << '\n';
+    }
+}
