8b15d82ee6f593686da5525e627125c66a7ab624
mhjmbs <mhjmbs@proton.me>
2024-02-24 16:55:03 -0300

    Add Yay! solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 342/A - Yay!.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    vector<int> freq(26, 0);
+
+    for(char c : s) freq[c-'a']++;
+
+    for(int i = 0; i < 26; i++) {
+        if(freq[i] == 1) cout << s.find('a'+i)+1 << '\n';
+    }
+}
