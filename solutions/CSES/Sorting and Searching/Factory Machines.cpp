4b551631f1e0f43758432dcd031abe2ee12b8e52
mhjmbs <mhjmbs@proton.me>
2024-02-27 17:28:48 -0300

    Refactor Factory Machines solution

--- solutions/CSES/Sorting and Searching/Factory Machines.cpp
+++ solutions/CSES/Sorting and Searching/Factory Machines.cpp
@@ -1,42 +1,46 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
 
-    ll n, t;
+    int n, t;
     cin >> n >> t;
 
-    vector<ll> k(n);
-    for(ll &ki : k) cin >> ki;
+    vector<int> k(n);
+    for(int& ki : k) cin >> ki;
 
-    auto check = [&k](ll curr_time) {
-        ll ans = 0;
-        for(ll &ki : k) {
-            ans += curr_time/ki;
-            if(ans < 0) return LONG_LONG_MAX;
-        }
-        return ans;
-    };
-
-    ll l = 0, r = 1e18, ans = -1;
+    ll l = 1, r = 1e18, ans = -1;
 
     while(l <= r) {
-        ll m = l + (r-l)/2;
+        ll mid = l + (r-l)/2;
 
-        ll curr_ans = check(m);
+        __int128_t produced = 0;
+        for(int ki : k) produced += mid/ki;
 
-        if(t <= curr_ans) {
-            ans = m;
-            r = m-1;
+        if(produced >= t) {
+            ans = mid;
+            r = mid-1;
         }else {
-            l = m+1;
+            l = mid+1;
         }
     }
 
