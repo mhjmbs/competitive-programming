9d0ca9f76c92748366f53f58286c51c9c255a382
mhjmbs <mhjmbs@proton.me>
2024-02-27 17:08:36 -0300

    Refactor Room Allocation solution

--- solutions/CSES/Sorting and Searching/Room Allocation.cpp
+++ solutions/CSES/Sorting and Searching/Room Allocation.cpp
@@ -1,11 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
 using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -13,39 +25,34 @@ int main() {
     int n;
     cin >> n;
 
-    vector<tiii> customers(n);
-    for(int i = 0, a, b; i < n; i++) {
-        cin >> a >> b;
-        customers[i] = {a,b,i};
-    }
-
-    sort(customers.begin(),customers.end());
-
     multiset<pii> rooms;
-    int next_room = 1;
-
-    vector<int> ans(n);
-
-    for(tiii c : customers) {
-        int room = next_room;
-        auto it = rooms.lower_bound(make_pair(get<0>(c), INT_MIN));
-        if(it != rooms.begin()) it--;
-        if(it != rooms.end() && it->first < get<0>(c)) {
-            room = it->second;
-            rooms.erase(it);
-        }else {
-            next_room++;
-        }
-        rooms.emplace(get<1>(c), room);
-
-        ans[get<2>(c)] = room;
-    }
-
-    cout << rooms.size() << '\n';
+    vector<tiii> intervals(n);
 
     for(int i = 0; i < n; i++) {
-        cout << ans[i];
-        if(i != n-1) cout << ' ';
-        else cout << '\n';
+        int l, r;
+        cin >> l >> r;
+        intervals[i] = {l,r, i};
     }
+
+    sort(intervals.begin(), intervals.end());
+
+    int nextRoom = 1;
+    vector<int> ans(n);
+
+    for(auto [l,r,i] : intervals) {
+        int room;
+        if(rooms.empty() || l <= rooms.begin()->first) {
+            room = nextRoom++;
+        }else {
+            room = rooms.begin()->second;
+            rooms.erase(rooms.begin());
+        }
+        rooms.emplace(r, room);
+        ans[i] = room;
+    }
+
+    cout << nextRoom-1 << '\n';
+
+    for(int x : ans) cout << x << ' ';
+    cout << '\n';
 }
