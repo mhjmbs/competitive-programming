d50db0979e04f2112e33bc3dcc17ecc5c9b45eaa
mhjmbs <mhjmbs@proton.me>
2024-02-27 16:14:22 -0300

    Add Turtle Tenacity: Continual Mods solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 929 (Div. 3)/D - Turtle Tenacity: Continual Mods.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int& ai : a) cin >> ai;
+
+        sort(a.begin(), a.end());
+
+        bool yes = false;
+
+        if(a[0] != a[1]) yes = true;
+        else {
+            for(int ai : a) if(ai % a[0] != 0) yes = true;
+        }
+
+        cout << (yes ? "YES\n" : "NO\n");
+    }
+}
