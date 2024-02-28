1ce74f3caa10616f32fe13e74509fc419cb0f70c
mhjmbs <mhjmbs@proton.me>
2024-02-28 01:13:18 -0300

    Refactor Sum of Three Values solution

--- solutions/CSES/Sorting and Searching/Sum of Three Values.cpp
+++ solutions/CSES/Sorting and Searching/Sum of Three Values.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,29 +25,25 @@ int main() {
     int n, x;
     cin >> n >> x;
 
-    vector<int> a(n);
-    for(int &ai : a) cin >> ai;
-    
-    vector<pii> s(n);
+    vector<pii> a(n);
+
     for(int i = 0; i < n; i++) {
-        s[i] = {a[i], i+1}; 
+        cin >> a[i].first;
+        a[i].second = i+1;
     }
 
-    sort(s.begin(), s.end());
+    sort(a.begin(), a.end());
 
-    int ans[3] = {-1,-1,-1};
+    array<int,3> ans = {-1,-1,-1};
 
-    for(int i = 0; i < n-2; i++) {
-        for(int j = i+1; j < n-1; j++) {
-            auto it = lower_bound(s.begin()+j+1, s.end(), make_pair(x-(s[i].first+s[j].first), INT_MIN));
-            if(it != s.end() && it->first == x-(s[i].first+s[j].first)) {
-                ans[0] = s[i].second;
-                ans[1] = s[j].second;
-                ans[2] = it->second;
-            } 
+    for(int i = 0; i < n; i++) {
+        for(int j = i+1; j < n; j++) {
+            int curr = a[i].first + a[j].first;
+            int k = lower_bound(a.begin()+j+1, a.end(), make_pair(x-curr,-1)) - a.begin();
+            if(k != n && a[k].first == x-curr) ans = {a[i].second, a[j].second, a[k].second};
         }
     }
 
     if(ans[0] == -1) cout << "IMPOSSIBLE\n";
-    else cout << ans[0] << ' ' << ans[1] << ' ' << ans[2] << '\n'; 
+    else cout << ans[0] << ' ' << ans[1] << ' ' << ans[2] << '\n';
 }
