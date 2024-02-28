e7a592d944e71c28b9d7eb93c406a5235373cf59
mhjmbs <mhjmbs@proton.me>
2024-02-28 00:02:44 -0300

    Add Binary Supersonic Utahraptors solution

---
+++ solutions/Baekjoon/Binary Supersonic Utahraptors.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int n, m, k;
+    cin >> n >> m >> k;
+
+    vector<int> a(n);
+    for(int& ai : a) cin >> ai;
+
+    vector<int> b(m);
+    for(int& bi : b) cin >> bi;
+
+    while(k--) {
+        int ki;
+        cin >> ki;
+    }
+
+    cout << abs( count_if(a.begin(), a.end(), [](int x) {return x == 0;}) - count_if(b.begin(), b.end(), [](int x) {return x == 1;}) ) << '\n';
+}
