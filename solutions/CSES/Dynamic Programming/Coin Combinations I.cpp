4efd1311fc1e668faee85325f38fa569a9785d43
mhjmbs <mhjmbs@proton.me>
2024-03-01 10:36:20 -0300

    Refactor Coin Combinations I solution

--- solutions/CSES/Dynamic Programming/Coin Combinations I.cpp
+++ solutions/CSES/Dynamic Programming/Coin Combinations I.cpp
@@ -1,24 +1,41 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
-    
+    fastio;
+
     int n, x;
     cin >> n >> x;
 
-    vector<int> coin(n), dp(x+1, 0);
-    for(int& c : coin) cin >> c;
+    int m = 1e9+7;
 
+    vector<int> c(n);
+    for(int& ci : c) cin >> ci;
+
+    vector<ll> dp(x+1, 0);
     dp[0] = 1;
 
     for(int i = 1; i <= x; i++) {
-        for(int c : coin) {
-            if(i-c >= 0) {
-                dp[i] += dp[i-c];
-                dp[i] %= int(1e9) + 7;
-            }
+        for(int ci : c) {
+            if(i-ci >= 0) dp[i] = (dp[i] + dp[i-ci]) % m;
         }
     }
 
