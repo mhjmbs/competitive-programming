3d7aa46afd1e5be50ca00946dbeba2f7f4920f00
mhjmbs <mhjmbs@proton.me>
2024-03-01 10:21:57 -0300

    Refactor Dice Combinations solution

--- solutions/CSES/Dynamic Programming/Dice Combinations.cpp
+++ solutions/CSES/Dynamic Programming/Dice Combinations.cpp
@@ -1,22 +1,38 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     int n;
     cin >> n;
 
-    vector<int> dp(n+1, 0);
+    ll m = 1e9+7;
+
+    vector<ll> dp(n+1, 0);
     dp[0] = 1;
 
     for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= 6; j++) {
-            if(i-j >= 0) {
-                dp[i] += dp[i-j];
-                dp[i] %= int(1e9) + 7;
-            }
+        for(int d = 1; d <= 6; d++) {
+            if(i-d >= 0) dp[i] = (dp[i] + dp[i-d]) % m;
         }
     }
 
