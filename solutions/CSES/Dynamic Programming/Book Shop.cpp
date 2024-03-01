131614a0768f4270671d92812f7a4b526da1e1a6
mhjmbs <mhjmbs@proton.me>
2024-03-01 11:11:30 -0300

    Refactor Book Shop solution

--- solutions/CSES/Dynamic Programming/Book Shop.cpp
+++ solutions/CSES/Dynamic Programming/Book Shop.cpp
@@ -1,29 +1,43 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
-    ios::sync_with_stdio(0);
+    fastio;
 
     int n, x;
     cin >> n >> x;
 
-    vector<int> price(n+1), pages(n+1);
+    vector<int> h(n);
+    for(int& hi : h) cin >> hi;
 
-    for(int b = 1; b < n+1; b++) cin >> price[b];
-    for(int b = 1; b < n+1; b++) cin >> pages[b];
+    vector<int> s(n);
+    for(int& si : s) cin >> si;
 
-    vector<vector<int>> dp(n+1, vector<int>(x+1, 0));
+    vector<int> dp(x+1, 0);
 
-    for(int b = 1; b <= n; b++) {
-        for(int s = 1; s <= x; s++) {
-            dp[b][s] = dp[b-1][s];
-
-            if(s-price[b] >= 0) {
-                dp[b][s] = max(dp[b][s], dp[b-1][s-price[b]] + pages[b]);
-            }
+    for(int i = 0; i < n; i++) {
+        for(int p = x; p >= 0; p--) {
+            if(p - h[i] >= 0) dp[p] = max(dp[p], dp[p-h[i]] + s[i]);
         }
     }
 
-    cout << dp[n][x] << '\n';
+    cout << *max_element(dp.begin(), dp.end()) << '\n';
 }
