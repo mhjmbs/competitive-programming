5fe1026315f884347166531700248cc327e4cd2d
mhjmbs <mhjmbs@proton.me>
2024-03-01 10:16:17 -0300

    Refactor Maximum Subarray Sum II solution

--- solutions/CSES/Sorting and Searching/Maximum Subarray Sum II.cpp
+++ solutions/CSES/Sorting and Searching/Maximum Subarray Sum II.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -12,33 +25,24 @@ int main() {
     int n, a, b;
     cin >> n >> a >> b;
 
-    vector<ll> prefix(n+1, 0);
-    for(int i = 1, inp; i <= n; i++) {
-        cin >> inp;
-        prefix[i] = prefix[i-1] + inp;
-    }
+    vector<int> x(n);
+    for(int& xi : x) cin >> xi;
 
-    ll start = 0, end = b, ans = LONG_LONG_MIN;
-    map<ll,ll> occur;
-    occur[0]++;
+    vector<ll> pref(n+1, 0);
+    for(int i = 1; i <= n; i++) pref[i] = pref[i-1] + x[i-1];
 
-    int next_prefix = 1;
+    ll ans = LONG_LONG_MIN;
 
-    for(int start = 0, end = a; end <= n;) {
-        ans = max(ans, prefix[end] - (*occur.begin()).first);
+    int l = -1 -(b-a), r = -1, curr = a;
+    multiset<ll> occur;
 
-        if(end == n) break;
-
-        end++;
-
-        if(end-start > b) {
-            occur[prefix[start]]--;
-            if(occur[prefix[start]] == 0) occur.erase(prefix[start]);
-            start++;
-        }
-
-        occur[prefix[next_prefix]]++;
-        next_prefix++;
+    while(curr <= n) {
+        r++;
+        occur.insert(pref[r]);
+        if(l >= 0) occur.erase(occur.lower_bound(pref[l]));
+        l++;
+        ans = max(ans, pref[curr] - *occur.begin());
+        curr++;
     }
 
     cout << ans << '\n';
