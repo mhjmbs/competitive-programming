5efc1267cef67f5d14b80df3b29497c9e68c8baa
mhjmbs <mhjmbs@proton.me>
2024-03-01 07:48:01 -0300

    Refactor Subarray Distinct Values solution

--- solutions/CSES/Sorting and Searching/Subarray Distinct Values.cpp
+++ solutions/CSES/Sorting and Searching/Subarray Distinct Values.cpp
@@ -1,10 +1,23 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
 
 int main() {
     fastio;
@@ -13,21 +26,25 @@ int main() {
     cin >> n >> k;
 
     vector<int> x(n);
-    for(int &xi : x) cin >> xi;
+    for(int& xi : x) cin >> xi;
 
     map<int,int> occur;
+
     ll ans = 0;
+    int l = 0, r = 0;
 
-    for(int start = 0, end = 0; end < n; end++) {
-        occur[x[end]]++;
-
+    while(r < n) {
+        occur[x[r]]++;
+    
         while(occur.size() > k) {
-            occur[x[start]]--;
-            if(occur[x[start]] == 0) occur.erase(x[start]);
-            start++;
+            occur[x[l]]--;
+            if(occur[x[l]] == 0) occur.erase(x[l]);
+            l++;
         }
 
-        ans += end-start+1;
+        ans += r-l+1;
+
+        r++;
     }
 
     cout << ans << '\n';
