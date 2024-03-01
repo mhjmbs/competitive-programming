93ba82414c143f3cf8e6c63d9bfd1ac7f8a1b0d2
mhjmbs <mhjmbs@proton.me>
2024-03-01 07:38:22 -0300

    Refactor Subarray Divisibility solution

--- solutions/CSES/Sorting and Searching/Subarray Divisibility.cpp
+++ solutions/CSES/Sorting and Searching/Subarray Divisibility.cpp
@@ -1,10 +1,25 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+ll Mod(ll a, ll m) {
+    return (a%m + m) % m;
+}
 
 int main() {
     fastio;
@@ -13,22 +28,18 @@ int main() {
     cin >> n;
 
     vector<int> a(n);
-    for(int &ai : a) cin >> ai;
+    for(int& ai : a) cin >> ai;
 
-    vector<ll> prefix(n+1, 0);
-    for(int i = 1; i <= n; i++) prefix[i] = prefix[i-1] + a[i-1];
+    vector<ll> pref(n+1, 0);
+    for(int i = 1; i <= n; i++) pref[i] = Mod(pref[i-1] + a[i-1],n);
 
-    map<ll,ll> occur;
-    occur[0] = 1;
+    map<int,int> occur;
 
     ll ans = 0;
 
-    for(int i = 1; i <= n; i++) {
-        int toFind = prefix[i] % n;
-        if(toFind < 0) toFind += n;
-        auto it = occur.find(toFind);
-        if(it != occur.end()) ans += it->second;
-        occur[toFind]++;
+    for(int i = 0; i <= n; i++) {
+        ans += occur[pref[i]];
+        occur[pref[i]]++;
     }
 
     cout << ans << '\n';
