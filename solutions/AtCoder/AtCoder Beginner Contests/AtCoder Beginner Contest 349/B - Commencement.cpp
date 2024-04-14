2f34ca7278de40d0c9b9a2801f2b41df51cb959e
mhjmbs <mhjmbs@proton.me>
2024-04-14 13:56:40 -0300

    Add Commencement solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 349/B - Commencement.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    vector<int> occur(26, 0);
+
+    for(char c : s) occur[c-'a']++;
+
+    vector<int> occur2(s.size()+1, 0);
+
+    for(int i = 0; i < 26; i++) {
+        occur2[occur[i]]++;
+    }
+
+    if(all_of(occur2.begin()+1, occur2.end(), [](int x) {return x == 2 || x == 0;})) {
+        cout << "Yes\n";
+    }else {
+        cout << "No\n";
+    }
+}
