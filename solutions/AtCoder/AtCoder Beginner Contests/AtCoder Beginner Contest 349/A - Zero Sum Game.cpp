a6d0ee57f7674916739d911f376098e8288ccb99
mhjmbs <mhjmbs@proton.me>
2024-04-14 13:56:28 -0300

    Add Zero Sum Game solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 349/A - Zero Sum Game.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    int n;
+    cin >> n;
+
+    vector<int> a(n+1);
+    for(int i = 1; i <= n-1; i++) cin >> a[i];
+
+    int score = 0;
+
+    for(int i = 1; i <= n; i++) {
+        score -= a[i];
+    }
+
+    cout << score << '\n';
+}
