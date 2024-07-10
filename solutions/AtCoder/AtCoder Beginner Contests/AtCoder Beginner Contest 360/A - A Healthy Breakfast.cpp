070602ba52b24e0bb18713070f48ae82f1cfc1a6
mhjmbs <mhjmbs@proton.me>
2024-07-09 21:00:06 -0300

    Add A Healthy Breakfast solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 360/A - A Healthy Breakfast.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    if(s[0] == 'R' || s[0] != 'M' && s[1] == 'R') cout << "Yes\n";
+    else cout << "No\n";
+}
