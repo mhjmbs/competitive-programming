cd04fc1f34cc9815beefa729415369532738fc82
mhjmbs <mhjmbs@proton.me>
2025-02-19 20:54:31 -0300

    Add Poisonous Oyster solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 393/A - Poisonous Oyster.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+int main() {
+    fastio;
+
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int ans = -1;
+
+    if(s1 == "sick" && s2 == "sick") ans = 1;
+    else if(s1 == "sick") ans = 2;
+    else if(s2 == "sick") ans = 3;
+    else ans = 4;
+    
+    cout << ans << '\n';
+}
