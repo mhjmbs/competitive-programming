eefcd96e80286e1fe0b396f182c126c49653868f
mhjmbs <mhjmbs@proton.me>
2025-04-16 19:25:28 -0300

    Refactor Finding a Centroid solution

--- solutions/CSES/Tree Algorithms/Finding a Centroid.cpp
+++ solutions/CSES/Tree Algorithms/Finding a Centroid.cpp
@@ -8,6 +8,7 @@ using namespace __gnu_pbds;
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
 using tiii = tuple<int,int,int>;
@@ -16,59 +17,44 @@ using tlll = tuple<ll,ll,ll>;
 using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
 using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
 
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 int n;
-vector<vector<int>> adjacencies;
-vector<int> subtreeSize;
-vector<bool> vis;
+vector<vector<int>> g;
+vector<int> c;
 
-int findCentroid(int node)
-{
-    vis[node] = true;
-    int centroid = node;
-
-    for(int adj : adjacencies[node])
-    {
-        if(!vis[adj] && subtreeSize[adj] > n/2)
-        {
-            centroid = findCentroid(adj);
-        }
+void dfs(int u, int p) {
+    c[u] = 1;
+    for(int v : g[u]) {
+        if(v == p) continue;
+        dfs(v, u);
+        c[u] += c[v];
     }
-
-    return centroid;
 }
 
-int computeSubtrees(int node)
-{
-    vis[node] = true;
-    subtreeSize[node] = 1;
-
-    for(int adj : adjacencies[node])
-    {
-        if(!vis[adj])
-        {
-            subtreeSize[node] += computeSubtrees(adj);
-        }
+int dfs2(int u, int p) {
+    for(int v : g[u]) {
+        if(v == p) continue;
+        if(c[v] > n/2) return dfs2(v, u);
     }
-
-    return subtreeSize[node];
+    return u;
 }
 
 int main() {
     fastio;
 
     cin >> n;
-    adjacencies.resize(n+1);
-    vis.resize(n+1, false);
-    subtreeSize.resize(n+1, 0);
 
-    for(int i = 0, a, b; i < n-1; i++)
-    {
-        cin >> a >> b;
-        adjacencies[a].push_back(b);
-        adjacencies[b].push_back(a);
+    g.resize(n+1);
+    for(int m = 0; m < n-1; m++) {
+        int u, v;
+        cin >> u >> v;
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
 
-    computeSubtrees(1);
-    fill(vis.begin(), vis.end(), false);
-    cout << findCentroid(1) << '\n';
+    c.resize(n+1);
+    dfs(1, -1);
+
+    cout << dfs2(1, -1) << '\n';
 }
