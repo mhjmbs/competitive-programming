04d40cf504171c5bc276a7b5922d76155a187c4d
mhjmbs <mhjmbs@proton.me>
2025-06-11 22:50:48 -0300

    Add Security 2 solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 407/C - Security 2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+int main() {
+    fastio;
+
+    string s;
+    cin >> s;
+
+    ll moves = 0;
+
+    for(int i = s.size()-1; i >= 0; i--) {
+        int curr = moves % 10;
+        int num = s[i]-'0';
+        moves += (num - curr + 10) % 10;
+    }
+
+    cout << moves+s.size() << '\n';
+}
