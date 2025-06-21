e9a938619db292375051b8d0feeb436cc811f1f3
mhjmbs <mhjmbs@proton.me>
2025-06-21 10:16:03 -0300

    Add Is it rated? solution

---
+++ solutions/AtCoder/AtCoder Beginner Contests/AtCoder Beginner Contest 405/A - Is it rated?.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+int main() {
+    fastio;
+
+    int r, x;
+    cin >> r >> x;
+
+    if(x == 1) {
+        cout << (1600 <= r && r <= 2999 ? "Yes\n" : "No\n");
+    }else {
+        cout << (1200 <= r && r <= 2399 ? "Yes\n" : "No\n");
+    }
+}
