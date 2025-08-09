4cf267cd619ce89a17b8b27f6b3a572012f26d31
mhjmbs <mhjmbs@proton.me>
2025-08-08 23:23:05 -0300

    Refactor Fiest element at least X solution

---
+++ solutions/Codeforces/ITMO Academy: pilot course/Segment Tree, part 1/Step 2/C - First element at least X.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template<typename T, auto Op, T e>
+struct SegmentTree {
+    vector<T> seg;
+    int l0, ln;
+
+    SegmentTree(int n) : ln(1) {
+        while(ln < n) ln *= 2;
+        seg.resize(2*ln-1, e);
+        l0 = seg.size() - ln;
+    }
+
+    SegmentTree(vector<T>& a) : SegmentTree(a.size()) {
+        copy(a.begin(), a.end(), seg.begin()+l0);
+        for(int i = l0-1; i >= 0; i--) {
+            seg[i] = Op(seg[2*i+1], seg[2*i+2]);
+        }
+    }
+
+    T get(int i) {
+        return seg[l0+i];
+    }
+
+    void set(int i, T x) {
+        i = l0+i;
+        seg[i] = x;
+        while(i > 0) {
+            i = (i-1)/2;
+            seg[i] = Op(seg[2*i+1], seg[2*i+2]);
+        }
+    }
+
+    T query(int l, int r) {
+        return query(l, r, 0, ln, 0);
+    }
+
+    T query(int l, int r, int lx, int rx, int i) {
+        if(r <= lx || rx <= l) return e;
+        if(l <= lx && rx <= r) return seg[i];
+        int m = (lx+rx)/2;
+        return Op( query(l, r, lx, m, 2*i+1), query(l, r, m, rx, 2*i+2) );
+    }
+
+    T lower_bound(int x) {
+        int i = 0;
+        while(i < l0) {
+            i = (seg[2*i+1] >= x) ? 2*i+1 : 2*i+2;
+        }
+        return seg[i] >= x ? i-l0 : -1;
+    }
+};
+
+int Max(int a, int b) {
+    return max(a,b);
+}
+
+int main() {
+    fastio;
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    for(int& ai : a) cin >> ai;
+
+    SegmentTree<int, Max, INT_MIN> seg(a);
+
+    while(m--) {
+        int mi;
+        cin >> mi;
+
+        if(mi == 1) {
+            int i, v;
+            cin >> i >> v;
+            seg.set(i, v);
+        }else {
+            int x;
+            cin >> x;
+            cout << seg.lower_bound(x) << '\n';
+        }
+    }
+}
