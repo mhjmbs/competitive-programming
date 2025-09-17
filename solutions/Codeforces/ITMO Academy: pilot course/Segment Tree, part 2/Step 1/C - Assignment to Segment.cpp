a0b3ac84806e75d2294b7f9bf27753aa0fc57162
mhjmbs <mhjmbs@proton.me>
2025-09-16 21:21:55 -0300

    Add Assignment to Segment solution

---
+++ solutions/Codeforces/ITMO Academy: pilot course/Segment Tree, part 2/Step 1/C - Assignment to Segment.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template<typename T, T e>
+struct SegmentTree {
+    vector<T> seg;
+    int l0, ln;
+ 
+    SegmentTree(int n) : ln(1) {
+        while(ln < n) ln *= 2;
+        seg.resize(2*ln-1, e);
+        l0 = seg.size() - ln;
+    }
+ 
+    SegmentTree(const vector<T>& a) : SegmentTree(a.size()) {
+        copy(a.begin(), a.end(), seg.begin()+l0);
+    }
+ 
+    T get(int pos) {
+        int i = 0, lx = 0, rx = ln;
+        while(i < l0) {
+            if(seg[i] != -1) {
+                seg[2*i+1] = seg[2*i+2] = seg[i];
+                seg[i] = -1;
+            }
+            int m = (lx+rx)/2;
+            if(pos < m) {
+                i = 2*i+1;
+                rx = m;
+            }else {
+                i = 2*i+2;
+                lx = m;
+            }
+        }
+        return seg[i];
+    }
+ 
+    void upd(int l, int r, T val) {
+        upd(l, r, val, 0, ln, 0);
+    }
+ 
+    void upd(int l, int r, T val, int lx, int rx, int i) {
+        if(r <= lx || rx <= l) return;
+        if(i < l0 && seg[i] != -1) {
+            seg[2*i+1] = seg[2*i+2] = seg[i];
+            seg[i] = -1;
+        }
+        if(l <= lx && rx <= r) {
+            seg[i] = val;
+            return;
+        }
+        int m = (lx+rx)/2;
+        upd(l, r, val, lx, m, 2*i+1), upd(l, r, val, m, rx, 2*i+2);
+    }
+};
+
+
+int main() {
+    fastio;
+
+    int n, m;
+    cin >> n >> m;
+
+    SegmentTree<int,-1> seg(vector<int>(n, 0));
+
+    while(m--) {
+        int type;
+        cin >> type;
+
+        if(type == 1) {
+            int l, r, v;
+            cin >> l >> r >> v;
+            seg.upd(l,r,v);
+        }else {
+            int i;
+            cin >> i;
+            cout << seg.get(i) << '\n';
+        }
+    }
+}
