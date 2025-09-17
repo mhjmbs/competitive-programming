c5f262268d950b7ed3afb91496c89eefeecabc5e
mhjmbs <mhjmbs@proton.me>
2025-09-17 12:46:25 -0300

    Update segment tree's identity element to a function that returns the identity element
    This allows more complex type usage, such as std::multiset for merge sort tree

--- library/data-structures/segment-tree.hpp
+++ library/data-structures/segment-tree.hpp
@@ -2,21 +2,21 @@
 
 #include "../contest/template.cpp"
 
-template<typename T, auto Op, T e>
+template<class T, auto op, auto e>
 struct SegmentTree {
     vector<T> seg;
     int l0, ln;
  
     SegmentTree(int n) : ln(1) {
         while(ln < n) ln *= 2;
-        seg.resize(2*ln-1, e);
+        seg.resize(2*ln-1, e());
         l0 = seg.size() - ln;
     }
  
     SegmentTree(const vector<T>& a) : SegmentTree(a.size()) {
         copy(a.begin(), a.end(), seg.begin()+l0);
         for(int i = l0-1; i >= 0; i--) {
-            seg[i] = Op(seg[2*i+1], seg[2*i+2]);
+            seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
     }
  
@@ -29,7 +29,7 @@ struct SegmentTree {
         seg[i] = x;
         while(i > 0) {
             i = (i-1)/2;
-            seg[i] = Op(seg[2*i+1], seg[2*i+2]);
+            seg[i] = op(seg[2*i+1], seg[2*i+2]);
         }
     }
  
@@ -38,9 +38,9 @@ struct SegmentTree {
     }
  
     T query(int l, int r, int lx, int rx, int i) {
-        if(r <= lx || rx <= l) return e;
+        if(r <= lx || rx <= l) return e();
         if(l <= lx && rx <= r) return seg[i];
         int m = (lx+rx)/2;
-        return Op( query(l, r, lx, m, 2*i+1), query(l, r, m, rx, 2*i+2) );
+        return op( query(l, r, lx, m, 2*i+1), query(l, r, m, rx, 2*i+2) );
     }
 };
