2541463f5e6c652dd702ea8d22194e8be1c63720
mhjmbs <mhjmbs@proton.me>
2025-11-10 22:12:04 -0300

    Refactor Static Range Sum Queries solution

--- solutions/CSES/Range Queries/Static Range Sum Queries.cpp
+++ solutions/CSES/Range Queries/Static Range Sum Queries.cpp
@@ -1,10 +1,25 @@
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
 
 #define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
 
-using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
 
 int main() {
     fastio;
@@ -12,16 +27,17 @@ int main() {
     int n, q;
     cin >> n >> q;
 
-    vector<ll> x(n+1, 0);
-    for(int i = 1; i <=n; i++) cin >> x[i];
+    vector<ll> x(n);
+    for(ll& xi : x) cin >> xi;
 
-    vector<ll> p(n+1, 0);
-    for(int i = 0; i <= n; i++) {
-        p[i] = p[i-1] + x[i];
+    vector<ll> pref(n+1);
+    for(int i = 1; i <= n; i++) {
+        pref[i] = pref[i-1] + x[i-1];
     }
 
-    for(int i = 0, l, r; i < q; i++) {
+    while(q--) {
+        int l, r;
         cin >> l >> r;
-        cout << p[r]-p[l-1] << '\n';
+        cout << pref[r] - pref[l-1] << '\n';
     }
 }
