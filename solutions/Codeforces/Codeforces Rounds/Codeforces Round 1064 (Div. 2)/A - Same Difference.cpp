25633fea70e3d6df9d3687d1adf1d13339e224f9
mhjmbs <mhjmbs@proton.me>
2025-11-16 18:50:58 -0300

    Add Same Difference solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 1064 (Div. 2)/A - Same Difference.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+
+        int ans = INT_MAX;
+
+        for(int i = 0; i < n; i++) {
+            string ns = s;
+            fill(ns.begin(), ns.begin()+i, s[i]);
+            if(all_of(ns.begin(), ns.end(), [&s,i](char c) {return c == s[i];})) {
+                ans = min(ans, int(i-count(s.begin(), s.begin()+i, s[i])));
+            }
+        }
+
+        cout << ans << '\n';
+    }
+}
