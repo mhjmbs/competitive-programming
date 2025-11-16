09ac8a503423bc3a50eb31fdb9f0c1ae28491cdd
mhjmbs <mhjmbs@proton.me>
2025-11-16 18:51:28 -0300

    Add Marble Council solution

---
+++ solutions/Codeforces/Codeforces Rounds/Codeforces Round 1064 (Div. 2)/D - Marble Council.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const ll m = 998244353;
+
+int main() {
+    fastio;
+
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for(int& ai : a) cin >> ai;
+
+        vector<int> freq(n+1);
+        for(int ai : a) freq[ai]++;;
+
+        vector<ll> dp(n+1);
+        dp[0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int sum = n; sum >= 0; sum--) {
+                if(sum-freq[i] >= 0) dp[sum] = (dp[sum] + freq[i]*dp[sum-freq[i]]) % m;
+            }
+        }
+
+        ll ans = 0;
+        for(int sum = *max_element(freq.begin()+1, freq.end()); sum <= n; sum++) {
+            ans = (ans+dp[sum]) % m;
+        }
+
+        cout << ans << '\n';
+    }
+}
